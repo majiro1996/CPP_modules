8a939d7dd230c8d5585a7fcc6384504d63e7afd9
Majiro <majiro1996@gmail.com>
2023-08-20 22:23:46 +0200

    02-03

--- 02_cpp/ex03/Point.cpp
+++ 02_cpp/ex03/Point.cpp
@@ -6,7 +6,7 @@
 /*   By: manujime <manujime@student.42malaga.com    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/08/15 10:48:50 by manujime          #+#    #+#             */
-/*   Updated: 2023/08/15 10:59:05 by manujime         ###   ########.fr       */
+/*   Updated: 2023/08/16 19:52:57 by manujime         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,5 +14,36 @@
 
 Point::Point(void)
 {
+    (Fixed) this->_x = 0;
+    (Fixed) this->_y = 0;
+    std::cout << "Point default constructor called" << std::endl;
+}
 
+Point::Point(float const x, float const y)
+{
+    (Fixed) this->_x = x;
+    (Fixed) this->_y = y;
+    std::cout << "Point constructor called" << std::endl;
+}
+
+Point::Point(Point const &source)
+{
+    std::cout << "Point copy constructor called" << std::endl;
+    *this = source;
+}
+
+Point &Point::operator=(Point const &source)
+{
+    std::cout << "Point assignment operator called" << std::endl;
+    if (this != &source)
+    {
+        (Fixed) this->_x = source._x;
+        (Fixed) this->_y = source._y;
+    }
+    return (*this);
+}
+
+Point::~Point(void)
+{
+    std::cout << "Point destructor called" << std::endl;
 }
